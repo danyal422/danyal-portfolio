fc9635020063e4dace97896d8f68acf9dd356ad2
danyal422 <danyalqaiser424@gmail.com>
2024-09-27 01:28:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for future enhancements or interactions
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Portfolio loaded!");
+    // Additional scripts can be added here
+});
